fc7833d55c40c7d1242ed3e8fdc50e9dded18924
funniray <funniray10@gmail.com>
2018-07-11 16:48:58 -0400

    Now it should release to the ore...

--- mixer-sponge/src/main/java/pro/kdray/funniray/mixer/MixerSponge.java
+++ mixer-sponge/src/main/java/pro/kdray/funniray/mixer/MixerSponge.java
@@ -28,7 +28,7 @@ import java.nio.file.Path;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
-@Plugin(id = "mixer-sponge", description = "A Mixer Interactive Plugin", name = "Mixer Interactive Plugin", version = "1.0")
+@Plugin(id = "mixer-sponge", description = "A Mixer Interactive Plugin", name = "Mixer Interactive Plugin", version = "@version@")
 public final class MixerSponge{
 
     @Inject
